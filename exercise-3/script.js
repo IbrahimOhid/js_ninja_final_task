94ab054e1968eb97808616f65e79eee4be78710d
ibrahimohid <ibrahimohid77@gmail.com>
2024-08-13 07:14:35 +0600

    exercise-6 ans 1 to 5 updated

--- exercise-3/script.js
+++ exercise-3/script.js
@@ -8,6 +8,8 @@ function myInfo(firstName, lastName) {
 }
 myInfo('Mohammad', 'Ibrahim');
 
+
+
 // Return
 function multiplication(num1, num2) {
     return num1 * num2;
@@ -33,26 +35,20 @@ console.log(typeof (myFunction(null)));
 console.log(typeof (myFunction('string')));
 console.log(typeof (myFunction(['array'])));
 
+
 //Ans: 5
 function printDay(day) {
-    const weekDay = [];
-    weekDay[1] = 'Saturday';
-    weekDay[2] = 'Sunday';
-    weekDay[3] = 'Monday';
-    weekDay[4] = 'Tuesday';
-    weekDay[5] = 'Wednesday';
-    weekDay[6] = 'Thursday';
-    weekDay[7] = 'Friday';
-    
-    return (day < 1 || day > 7) ? undefined : weekDay[day];
+    const weekDay = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    return weekDay[day-1];
 }
 console.log(printDay(4));
 console.log(printDay(7));
 console.log(printDay(41));
 
+
 //Ans: 6
-function lastElement([num1, num2, num3, num4]) {
-    return num4;
+function lastElement(num) {
+    return num [num.length-1];
 }
 console.log(lastElement([1, 2, 3, 4]));
 console.log(lastElement([1, 2, 3, 10]));
@@ -61,13 +57,12 @@ console.log(lastElement([]))
 
 //Ans: 7
 //createObj(1, 2, 3) //{x: 1, y: 2, z:3}
-function createObj(x, y, z) {
-    let differentValue = {
-        x: x,
-        y: y,
-        z: z
-    };
-    return differentValue;
+function createObj(num1, num2, num3){
+    return{
+        x: num1,
+        y: num2,
+        z: num3
+    }
 }
 console.log(createObj(1, 2, 3));
 console.log(createObj('a', 'b', 'c'))
---
+++ exercise-6/script.js
@@ -0,0 +1,60 @@
+// Ans: 1
+
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     num += 2;
+// }
+
+//=> Here the value of num is 1. Condition is given that the value of num will be less than or equal to 10. Then 2 will be added to the value of num.  So 1, 3, 5, 7, 9 results have been obtained.
+
+// Ans: 2
+
+// let num = 1
+// while(num <= 20) {
+// if(num % 4 === 0){
+// console.log(num);
+// }
+// num++;
+// }
+
+//=> Here the value of num is given as 1. The condition is given that the value of num will be less than or equal to 20. Then the value of num will be added to 1 and it is said that if the value of num is divided by 4 the quotient will be 0.  Then the numbers between 1-20 which are divided by 0 will show the result of num.  Since 4, 8, 12, 16, 20 are divided by 4, the quotient is 0.  So these are showing results
+
+// Ans: 3
+
+// let num = 100;
+// while(num < 150) {
+// console.log(num + 1);
+// num--;
+// }
+
+//=> output error or browser fix. because it's infinite loop.it's condition not false
+
+// Ans: 4
+
+// let num = 1;
+// while(num <= 100) {
+// if(num % 2 === 0){
+// console.log(num);
+// }
+// num++;
+// }
+
+// for(let i = 1; i <= 100; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// Ans: 5
+
+function repeat(n){
+    for(let i = 1; i <= n; i++){
+        for(let j = 1; j <= i; j++){
+            document.write('$')
+        }
+        document.write('<br>')
+    }
+}
+repeat(3);
+repeat(5)
