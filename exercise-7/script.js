bc640a8d4c953886e9f9583eb63411446271a1fa
ibrahimohid <ibrahimohid77@gmail.com>
2024-08-16 11:55:07 +0600

    exercsise-7 updated

--- exercise-7/script.js
+++ exercise-7/script.js
@@ -1,21 +1,47 @@
 // Ans: 1
-// function myFunction(a, b) {
-//   console.log(a.concat(b));
-// }
+function myFunction(a, b) {
+  if(a.includes(b)){
+    result = b + a;
+  }
+  else{
+   result = a + b;
+  }
+  return result;
+}
 
-// myFunction("cheese", "cake"); //'cheesecake'
-// myFunction("lips", "s"); //'slips'
-// myFunction("Java", "script"); //'Javascript'
-// myFunction(" think, therefore I am", "I");
+console.log(myFunction("cheese", "cake"));; //'cheesecake'
+console.log(myFunction("lips", "s")); //'slips'
+console.log(myFunction("Java", "script")); //'Javascript'
+console.log(myFunction(" think, therefore I am", "I")); // 'I think, therefore I am'
 
 // Ans: 2
-// function myFunction(a, b) {
-//     const result = a.splice(-3, 0, b);
-//     return result;
-// }
-// console.log(myFunction('1234567', '.')); //'1.234.567'
-// myFunction('abcde', '$') //'ab$cde'
-// myFunction('zxyzxyzxyzxyzxyz', 'w') 
+function myFunction(a, b) {
+  let element = a.split('');
+  for (let i = element.length-3; i >= 0; i -= 3) {
+    element.splice(i, 0, b);
+  }
+  let result = element.join('')
+  return result;
+}
+console.log(myFunction('1234567', '.')); //'1.234.567'
+console.log(myFunction('abcde', '$')); //'ab$cde'
+console.log(myFunction('zxyzxyzxyzxyzxyz', 'w')); //'zwxyzwxyzwxyzwxyzwxyz'
+
+// Ans: 3
+function myFunction(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+      let code = str.charCodeAt(i)
+      let increaseCode = code + 1;
+      let newLetter = String.fromCharCode(increaseCode)
+      newStr = newStr + newLetter
+  }
+  console.log(newStr)
+return newStr;
+}
+myFunction('bnchmf') //'coding'
+myFunction('bgddrd') //'cheese'
+myFunction('sdrshmf') //'testing'
 
 // Ans: 4
 function myFunction(a, n) {
@@ -64,4 +90,9 @@ console.log(people.slice(2));
 //8.
 console.log(people.indexOf("Anis"));
 //9. 
-console.log(people);
+console.log(people.indexOf("Foo"));
+//10.
+console.log(people.splice(2,1, "Elizabeth", "Bob"));
+//11.
+const withMiraz = people.concat('Miraz');
+console.log(withMiraz);
