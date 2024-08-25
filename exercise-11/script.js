34c38f900f12c0f61c972475ca394cf39df79dca
ibrahimohid <ibrahimohid77@gmail.com>
2024-08-25 19:02:01 +0600

    exercise-11 done

--- exercise-11/script.js
+++ exercise-11/script.js
@@ -1,71 +1,71 @@
 // Ans: 1
-// function placeInMiddle(arr, vals){
-//     arr.splice(1,0, ...vals)
-//     return arr;
+function placeInMiddle(arr, vals){
+    arr.splice(1,0, ...vals)
+    return arr;
 
-// }
-// console.log(placeInMiddle([1,2,6,7],[3,4,5])); // [1,2,3,4,5,6,7])
-// console.log(placeInMiddle([1],[3,4,5])) // [1,3,4,5])
-// console.log(placeInMiddle([1,6],[2,3,4,5])) // [1,2,3,4,5,6]
-// console.log(placeInMiddle([],[2,3,4,5])) // [2,3,4,5]
+}
+console.log(placeInMiddle([1,2,6,7],[3,4,5])); // [1,2,3,4,5,6,7])
+console.log(placeInMiddle([1],[3,4,5])) // [1,3,4,5])
+console.log(placeInMiddle([1,6],[2,3,4,5])) // [1,2,3,4,5,6]
+console.log(placeInMiddle([],[2,3,4,5])) // [2,3,4,5]
 
 // Ans:2
-// function joinArrays(...newArr){
-//   let result = [];
-//   return result.concat(...newArr);
-// }
-// console.log(joinArrays([1],[2],[3])); // [1,2,3]
-// console.log(joinArrays([1],[2],[3],[1],[2],[3])); // [1,2,3,1,2,3]
-// console.log(joinArrays([1,2,3],[4,5,6],[7,8,9])); // [1,2,3,4,5,6,7,8,9]
-// console.log(joinArrays([1],[3],[0],[7])); // [1,3,0,7]
+function joinArrays(...newArr){
+  let result = [];
+  return result.concat(...newArr);
+}
+console.log(joinArrays([1],[2],[3])); // [1,2,3]
+console.log(joinArrays([1],[2],[3],[1],[2],[3])); // [1,2,3,1,2,3]
+console.log(joinArrays([1,2,3],[4,5,6],[7,8,9])); // [1,2,3,4,5,6,7,8,9]
+console.log(joinArrays([1],[3],[0],[7])); // [1,3,0,7]
 
 // Ans:3
-// function sumEvenArgs(...numValue){
+function sumEvenArgs(...numValue){
 
-//     let result = 0;
-//     for(let i = 1; i <= numValue.length; i++){
-//         if(numValue[i] % 2 === 0){
-//             result += numValue[i];
-//         }
-//     }
-//     return result;
-// }
-// console.log(sumEvenArgs(1,2,3,4)); // 6
-// console.log(sumEvenArgs(1,2,6)); // 8
-// console.log(sumEvenArgs(1,2)); // 2
+    let result = 0;
+    for(let i = 1; i <= numValue.length; i++){
+        if(numValue[i] % 2 === 0){
+            result += numValue[i];
+        }
+    }
+    return result;
+}
+console.log(sumEvenArgs(1,2,3,4)); // 6
+console.log(sumEvenArgs(1,2,6)); // 8
+console.log(sumEvenArgs(1,2)); // 2
 
 // Ans: 4
-// const obj = {
-//   name: "samim",
-//   email: "samimfazlu091@gmail.com",
-//   address: {
-//     location: {
-//       country: {
-//         name: "Bangladesh",
-//         city: "Dhaka",
-//       },
-//     },
-//     getAddress() {
-//       return this.country.city + "-" + this.country.name;
-//     },
-//   },
-// };
+const obj = {
+  name: "samim",
+  email: "samimfazlu091@gmail.com",
+  address: {
+    location: {
+      country: {
+        name: "Bangladesh",
+        city: "Dhaka",
+      },
+    },
+    getAddress() {
+      return this.country.city + "-" + this.country.name;
+    },
+  },
+};
 
-// const {address: { location: {country: { city }}}} = obj;
-// console.log(city);
-
-// console.log(getAddress.bind(obj.address.location)());
+const{address:{location:{country:{city}, getAddress}}} = obj;
+console.log(city);
+console.log(getAddress.bind(obj.address.location)());
 
 // Ans: 5
+console.log(sum([1, 2, 3, 4]));
+
 function sum(...values) {
     let total = 0;
-    for(let i = 0; i < values.length; i++){
-        total += values[i];
+    for(let element of values[0]){
+        total += element;
     }
     return total;
 }
 
-console.log(sum([1, 2, 3, 4]));
 
 // Ans:6
 
