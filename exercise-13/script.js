01cfaad0f0a6c5fa56989b21434725c983b381e7
ibrahimohid <ibrahimohid77@gmail.com>
2024-08-31 22:55:57 +0600

    exercise-13 answer 1 updated

---
+++ exercise-13/script.js
@@ -0,0 +1,43 @@
+// Ans: 1
+//a => const container = document.getElementById('container');
+
+//b => const container = document.querySelector('#container');
+
+//c => const secondLi = document.querySelectorAll('.second');
+
+/*d => const olLiEle = document.querySelector('ol');
+const result = olLiEle.querySelector('.third'); */
+
+/* e =>const container = document.querySelector('#container');
+container.textContent = 'Hello !' */
+
+
+/*f => const footer = document.querySelector('.footer');
+footer.classList.add('main'); */
+
+/*g => const footer = document.querySelector('.footer');
+footer.classList.remove(); */
+
+/*h => const olList = document.querySelector('ol');
+const newLi = document.createElement('li');
+olList.appendChild(newLi); */
+
+/* i => const olList = document.querySelector('ol');
+const newLi = document.createElement('li');
+olList.appendChild(newLi); 
+newLi.textContent = 'four'; */
+
+/* j => const olList = document.querySelector('ol');
+const newLi = document.createElement('li');
+newLi.textContent = 'five';
+olList.appendChild(newLi);
+newLi.classList.add('five'); */
+
+// const olList = document.querySelector('ol');
+// olList.forEach((elm) => {
+  
+// })
+
+/* l => const footer = document.querySelector('.footer')
+footer.remove('div'); */
+
